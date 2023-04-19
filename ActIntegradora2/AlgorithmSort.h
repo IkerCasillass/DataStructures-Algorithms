12447bc5898795b20726651fb44bd8553d5c5cf8
IkerCasillass <>
2023-04-19 03:47:30 +0000

    Creando act2

---
+++ ActIntegradora2/AlgorithmSort.h
@@ -0,0 +1,31 @@
+#ifndef _ALGORITMO_SORT_H_
+#define _ALGORITMO_SORT_H_
+
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <vector>
+#include "Registro.h"
+
+class Registro; // forward declaration
+
+template <class T> class AlgorithmSort {
+private:
+  int partition(std::vector<T> &A, int low, int high, unsigned int &compara,
+                unsigned int &swap);
+
+public:
+  AlgorithmSort();
+  void printVector(std::vector<T> A);
+
+  void insertionSort(std::vector<T> &A, int n, unsigned int &compara,
+                     unsigned int &swap);
+
+  void quickSort(std::vector<T> &A, int low, int high, unsigned int &compara,
+                 unsigned int &swap);
+
+  int binarySearch(std::vector<Registro> &vectorSorted, int key, int &compara);
+
+};
+
+#endif // _ALGORITMO_SORT_H_
